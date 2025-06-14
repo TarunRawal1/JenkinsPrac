d0a9af6ee610a4c9e6914f7a72716511a2b1a0b6
Tarun Rawal <tarunrawal100@gmail.com>
2025-06-14 12:04:21 +0530

    updated test

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import '@testing-library/jest-dom';
 
 test('renders welcome message and prompt', () => {
   render(<App />);

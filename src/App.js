c9b541c4fbd04fac0cd684e278dc17323651c724
Tarun Rawal <tarunrawal100@gmail.com>
2025-06-14 10:05:40 +0530

    changes

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to the Store</h1>
+      </header>
+      <p>What are you looking for?</p>
+    </div>
+  );
+}
+
+export default App;
